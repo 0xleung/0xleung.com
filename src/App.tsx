8bf45184757a17db986dc8fb2f28ab9ae95b6a9a
JobinLeung <jobinleung@gmail.com>
2022-02-20 18:17:11 +0800

    feat: init profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
 // import logo from './logo.svg';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
 
 
 import './App.css';
 
 import Page1 from './pages/page1/Page1';
-import Counter from './pages/counter/Counter';
-
 
 
 const App = ():JSX.Element=> {
   return (
     <>
       <CssBaseline/>
-      <div className="App">
-      <header className="App-header">
-        <nav>
-          <Link to={'/'}>Page1</Link>
-          <span> | </span>
-          <Link to={'/counter'}>Counter</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="counter" element={<Counter />} />
         <Route path="/" element={<Page1 />} />
       </Routes>
-    </div>
     </>
   );
 }
--- src/pages/page1/Page1.tsx
+++ src/pages/page1/Page1.tsx
@@ -1,8 +1,164 @@
+import Box from '@mui/material/Box/Box';
+import { styled } from '@mui/system';
+
+import avatarImage from '../../images/avatar.jpeg'
+import avatarHoverImage from '../../images/avatar-h.jpeg'
+
+import twitterIcon from '../../images/twitter.svg'
+import githubIcon from '../../images/github.svg'
+import mediumIcon from '../../images/medium.svg'
+
+const CenterOuter = styled('div')({
+    maxWidth: '1140px',
+    margin: '0 auto',
+    color: '#111',
+    backgroundColor: '#fff',
+});
+
+const ITitle = styled('h1')({
+    textAlign: 'center',
+    margin: 0,
+    padding: 0,
+});
+
+const ISubTitle = styled('h3')({
+    textAlign: 'center',
+    margin: 0,
+    padding: 0,
+});
+
+const IText = styled('div')({
+    textAlign: 'center',
+});
+
+const IAvatar = styled('div')({
+    width: '235px',
+    height: '235px',
+    margin: '0 auto',
+    background: `#eee url(${avatarImage}) center center no-repeat`,
+    ":hover": {
+        backgroundImage: `url(${avatarHoverImage})`,
+    },
+    backgroundSize: 'cover',
+    borderRadius: '50%',
+});
+
+const IAbout = styled('div')({
+    maxWidth: '600px',
+    width: '90vw',
+    margin: '0 auto',
+    color: '#fff',
+    backgroundColor: '#4e5dab',
+    padding: '20px',
+});
+
+export type SizedBoxProps = {
+    width?: string,
+    height?: string,
+    children?: JSX.Element | JSX.Element[] | string
+}
+
+export const SizedBox = (props: SizedBoxProps)=>{
+    const {width, height, children,} = props;
+    return (
+              <Box sx={{
+                  width: width ?? 0,
+                  height: height ?? 0,
+                  display: 'inline-block',
+              }}>
+                  {children}
+              </Box>
+    )
+}
+
+export type SocialLinkProps = {
+    image?: string,
+    path?: string,
+}
+
+export const SocialLink = (props: SocialLinkProps)=>{
+    const {image, path,} = props;
+    return (
+            <a href={path} target="_blank" rel="noopener noreferrer">
+                <Box sx={{
+                    width: '24px',
+                    height: '24px',
+                    margin: '0 auto',
+                    display: 'inline-block',
+                    background: `url(${image}) center center no-repeat`,
+                    ":hover": {
+                        opacity: 0.7,
+                    },
+                    backgroundSize: 'cover',
+                    borderRadius: '50%',
+                }} />
+            </a>
+    )
+}
+
 const Page1 = ():JSX.Element=>{
 
     return (
         <>
-            <h1>Boo Store</h1>
+            <CenterOuter>
+                <SizedBox height='50px' />
+                <ITitle >Profile</ITitle>
+                <SizedBox height='30px' />
+                <IText>
+                    I'm a fullstack developer
+                </IText>
+                <SizedBox height='30px' />
+                <IAvatar/>
+                <SizedBox height='30px' />
+                <IAbout>
+                    <ITitle >About me</ITitle>
+                    <IText>
+                    I'm a full stack developer and I'm very interested in crypto right now, if you have some really cool ideas, you can contact me via the link below.
+                    </IText>
+                    <SizedBox height='10px' />
+                </IAbout>
+                <SizedBox height='30px' />
+                <ITitle >Details</ITitle>
+                <SizedBox height='5px' />
+                <ISubTitle >Name:</ISubTitle>
+                <IText>
+                    Jobin Leung
+                </IText>
+                <SizedBox height='5px' />
+                <ISubTitle >Age:</ISubTitle>
+                <IText>
+                    34
+                </IText>
+                <SizedBox height='5px' />
+                <ISubTitle >Location:</ISubTitle>
+                <IText>
+                    Earth
+                </IText>
+                <SizedBox height='50px' />
+                <Box style={{
+                    textAlign: 'center',
+                }}>
+                    <SocialLink image={twitterIcon} path="https://twitter.com/jobinleung" />
+                    <SizedBox width='10px' />
+                    <SocialLink image={githubIcon} path="https://github.com/jobinleung" />
+                    <SizedBox width='10px' />
+                    <SocialLink image={mediumIcon} path="https://medium.com/@jobinleung" />
+                </Box>
+                <SizedBox height='50px' />
+            </CenterOuter>
+
+            <footer style={{
+                backgroundColor: '#333333',
+                color: '#fff',
+                padding: '50px',
+                textAlign: 'center',
+                fontSize: '12px',
+            }}>
+                <IText>
+                    created by Jobin with ♥
+                </IText>
+
+            </footer>
         </>
     );
 }
